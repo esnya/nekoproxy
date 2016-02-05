ec4bbb2cdca8a9d1d1b12f1017f191d0669e2a49
ukatama <dev.ukatama@gmail.com>
2016-02-05 15:57:12 +0900

    Watch etcd after get

--- src/server/etcd.js
+++ src/server/etcd.js
@@ -1,16 +1,55 @@
 import config from 'config';
+import { getLogger } from 'log4js';
 import request from 'request-promise';
+
+const logger = getLogger('[etcd]');
 
 const {
     hostname,
     port,
 } = config.get('etcd');
 
-export const get = (target) =>
+const cache = {};
+
+const watch = (target) => {
+    logger.info('Watching', target);
+
     request({
+        uri: `http://${hostname}:${port}/v2/keys/${target}?wait=true`,
+        json: true,
+    })
+    .then((res) => {
+        const value = res.node.value;
+
+        logger.info('Update', target, cache[target], ' > ', value);
+
+        cache[target] = value;
+
+        return watch(target);
+    });
+};
+
+export const get = (target) => {
+    if ((target in cache) && cache[target]) {
+        return Promise.resolve({
+            target: `http://${cache[target]}`,
+        });
+    }
+
+    logger.info('Get', target);
+
+    return request({
         uri: `http://${hostname}:${port}/v2/keys/${target}`,
         json: true,
     })
-    .then((res) => Promise.resolve({
-        target: `http://${res.node.value}`,
-    }, res));
+    .then((res) => {
+        const value = res.node.value;
+
+        cache[target] = value;
+        watch(target);
+
+        return {
+            target: `http://${value}`,
+        };
+    });
+};
